915d15a573da372f0857833f4235166921621149
jasaga2401 <jasaga@protonmail.com>
2024-10-10 15:57:13 +0100

    Add project files.

---
+++ exampleMathFactorial/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculateFactorial = New System.Windows.Forms.Button()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalculateFactorial
+        '
+        Me.btnCalculateFactorial.Location = New System.Drawing.Point(271, 249)
+        Me.btnCalculateFactorial.Name = "btnCalculateFactorial"
+        Me.btnCalculateFactorial.Size = New System.Drawing.Size(197, 72)
+        Me.btnCalculateFactorial.TabIndex = 0
+        Me.btnCalculateFactorial.Text = "Factorial"
+        Me.btnCalculateFactorial.UseVisualStyleBackColor = True
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumber.Location = New System.Drawing.Point(271, 180)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(197, 38)
+        Me.txtNumber.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.btnCalculateFactorial)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculateFactorial As Button
+    Friend WithEvents txtNumber As TextBox
+End Class
---
+++ exampleMathFactorial/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub btnCalculateFactorial_Click(sender As Object, e As EventArgs) Handles btnCalculateFactorial.Click
+
+        Dim num As Integer = Convert.ToInt32(txtNumber.Text)
+
+        ' Connection string to connect to MySQL database
+        Dim connectionString As String = "Server=localhost;Database=dbFactorial;User ID=root;Password=12Yellow34!"
+
+        ' Create a new MySQL connection
+        Using conn As New MySqlConnection(connectionString)
+            Try
+                ' Open the connection
+                conn.Open()
+
+                ' Create a MySQL command to call the stored procedure
+                Using cmd As New MySqlCommand("calculate_factorial", conn)
+                    cmd.CommandType = CommandType.StoredProcedure
+
+                    ' Add input parameter for the number (num)
+                    cmd.Parameters.AddWithValue("@num", num)
+
+                    ' Add output parameter for the result (factorial)
+                    Dim resultParam As New MySqlParameter("@result", MySqlDbType.Int64)
+                    resultParam.Direction = ParameterDirection.Output
+                    cmd.Parameters.Add(resultParam)
+
+                    ' Execute the stored procedure
+                    cmd.ExecuteNonQuery()
+
+                    ' Retrieve and display the result
+                    Dim result As Long = Convert.ToInt64(resultParam.Value)
+                    MessageBox.Show("The factorial of " & num.ToString() & " is: " & result.ToString())
+                End Using
+            Catch ex As MySqlException
+                MessageBox.Show("Error: " & ex.Message)
+            Finally
+                ' Close the connection
+                conn.Close()
+            End Try
+        End Using
+
+    End Sub
+End Class
